101e6e97b76e542c1ad1dc468e80454a1099e058
bariabbassi <abbassibari@gmail.com>
2023-10-04 14:09:50 +0100

    update layout.js

--- app/layout.js
+++ app/layout.js
@@ -31,8 +31,8 @@ export default function RootLayout({ children }) {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#011FA4" />
+        <meta name="theme-color" content="#011FA4" />
       </head>
       <body
         className={
