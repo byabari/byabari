3aad84aa791f1e2e54be67ae086425c5f7d8aaff
bariabbassi <abbassibari@gmail.com>
2023-10-17 16:05:09 +0100

    update page.js and Link.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,49 +1,58 @@
 import Image from "next/image";
+import Link from "@/components/Link";
 
 export default function About() {
   return (
     <>
       <h1 className="text-4xl font-bold mt-24 mb-8">About</h1>
       <div className="space-y-5 mb-12">
-        <p>
+        <div>
           Hi, my name is Bari. I was born in Amsterdam and moved to Morocco at
           the age of 10. I studied Computer Science in Paris and did an exchange
           semester in Japan. During my time at university, I worked at a
           different startup every summer.
-        </p>
-        <p>
+        </div>
+        <div>
           After my final semester in Japan, I joined Hubs, an Amsterdam-based
           scale-up, during its rapid growth phase. Hubs is an on-demand
           manufacturing marketplace with manufacturing partners around the
           world. I mainly worked on solving the problem of shipping delays.
           During my time there, the company was acquired for $280 million and
           rebranded from 3D Hubs to Hubs.
-        </p>
-        <p>
+        </div>
+        <div>
           Following that, I moved from Amsterdam to Tangier, Morocco, and joined
           the fully remote startup, Practice, after their seed round led by
           a16z. Practice is an all-in-one SaaS product for client-based
           businesses, such as coaches and consultants. As a full-stack
           developer, I worked on several core features, including the home
           dashboard, payments, onboarding flows, email notifications, etc.
-        </p>
-        <p>
+        </div>
+        <div>
           Apart from my computer work, I'm also passionate about photography.
-          You can check out my work on Instagram (@bariabbassi). Some of my
-          previous clients include Nokia, VanMoof, and El Fenn Hotel.
-        </p>
-        <p>
+          You can check out my work on Instagram (
+          <Link
+            className="inline"
+            href="https://instagram.com/bariabbassi"
+            target="_blank"
+          >
+            @bariabbassi
+          </Link>
+          ). Some of my previous clients include Nokia, VanMoof, and El Fenn
+          Hotel.
+        </div>
+        <div>
           My lifestyle also includes regular biking and swimming. One of the
           driving factors behind my move to Tangier was the opportunity to
           pursue these hobbies year-round. Back in my teenage years, I competed
           in numerous cycling races and achieved a notable 7th place at the
           Moroccan cycling championships. These days, I continue to take part in
           the occasional triathlon or cycling race every few months.
-        </p>
-        <p>
+        </div>
+        <div>
           You can contact me at:{" "}
           <a href="mailto:bari@abbas.si">bari@abbas.si</a>
-        </p>
+        </div>
       </div>
       <Image src="/bari_abbassi.jpg" width={400} height={400} />
     </>
--- components/Link.js
+++ components/Link.js
@@ -3,7 +3,9 @@ import NextLink from "next/link";
 export default function Link(props) {
   return (
     <NextLink {...props}>
-      <div className="hover:underline underline-offset-4">{props.children}</div>
+      <div className={"hover:underline underline-offset-4 " + props.className}>
+        {props.children}
+      </div>
     </NextLink>
   );
 }
