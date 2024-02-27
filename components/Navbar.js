1f91fde7f1ea06de1485b5dfd20c52a9f60a2d9f
bariabbassi <abbassibari@gmail.com>
2024-02-27 13:08:21 +0100

    update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="w-full">
       <div className="flex items-center justify-between pb-1.5">
-        <Link href="/">byabari.com</Link>
+        <Link href="/">by abari</Link>
         <div className="flex items-center space-x-4">
           <Link href="/about">About</Link>
         </div>
