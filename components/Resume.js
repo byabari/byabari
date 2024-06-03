b9aa346a9762a33032a48b77f7800eb65e62b9a4
bariabbassi <abbassibari@gmail.com>
2024-06-03 13:37:36 +0100

    update Resume.js

--- components/Resume.js
+++ components/Resume.js
@@ -39,7 +39,7 @@ export default function Resume() {
           companyWebsite="https://hubs.com"
           jobDescription="Hubs is an on-demand manufacturing marketplace. I joined Hubs at the
           peak of their rapid growth stage. During my time there, the company
-          was acquired for $280 million by Protolabs."
+          was acquired for $300 million by Protolabs."
           jobTitle="Frontend Engineer"
           startDate="2020"
           endDate="2021"
