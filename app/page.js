073ab1c92bb0801c7e7eb13a66706cf9f524ca4d
bariabbassi <abbassibari@gmail.com>
2024-07-02 02:24:52 +0100

    update page.js

--- app/page.js
+++ app/page.js
@@ -61,8 +61,9 @@ export default function HomePage() {
       >
         I was born in Amsterdam, gew up between Morocco and Amsterdam, studied
         Computer Science in Paris, and did an exchange semester in Japan. My web
-        framework of choice is React. Among the companies I've worked for are{" "}
-        <Link href="https://practice.do/">Practice</Link> and{" "}
+        framework of choice is React. I'm currently based in Tangier, Morocco,
+        and work with companies from around the world. Among the companies I've
+        worked for are <Link href="https://practice.do/">Practice</Link> and{" "}
         <Link href="https://hubs.com/">Hubs</Link>.
       </p>
       <Image
