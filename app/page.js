ef868f98cae13e7a437a20f9043d8bac2dfe55a4
bariabbassi <abbassibari@gmail.com>
2024-07-03 04:25:03 +0100

    update page.js

--- app/page.js
+++ app/page.js
@@ -62,8 +62,8 @@ export default function HomePage() {
         I was born in Amsterdam, gew up between Morocco and Amsterdam, studied
         Computer Science in Paris, and did an exchange semester in Japan. I'm
         currently based in Tangier, Morocco, and work with companies from around
-        the world. My web framework of choice is React. Among the companies I've
-        worked for are <Link href="https://practice.do/">Practice</Link> and{" "}
+        the world. Among the companies I've worked for are{" "}
+        <Link href="https://practice.do/">Practice</Link> and{" "}
         <Link href="https://hubs.com/">Hubs</Link>.
       </p>
       <Image
@@ -97,7 +97,7 @@ export default function HomePage() {
         <Link href="https://linkfolders.com/">Linkfolders</Link>.
       </p>
       <h2 className={instrumentSerif.className + " text-3xl sm:text-4xl mb-7"}>
-        I also post travel content on{" "}
+        I also post stuff on{" "}
         <span className="italic leading-tighter">Instagram</span>
       </h2>
       <div className="flex flex-wrap gap-2 mb-7">
